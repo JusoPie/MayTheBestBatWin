5ac61f7f601228359f04c188c569a4cb0c306464
JusoPie <128028155+JusoPie@users.noreply.github.com>
2024-10-09 12:23:03 +0300

    edistyyy

--- Assets/Scripts/BackgroundFollow.cs
+++ Assets/Scripts/BackgroundFollow.cs
@@ -11,7 +11,7 @@ public class BackgroundDrop : MonoBehaviour
         Vector3 cameraPosition = mainCamera.transform.position;
 
         // Update the position to follow the camera's X position and drop down
-        Vector3 newPosition = new Vector3(cameraPosition.x, transform.position.y - dropSpeed * Time.deltaTime, transform.position.z);
+        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y - dropSpeed * Time.deltaTime, transform.position.z);
 
         // Apply the new position
         transform.position = newPosition;
