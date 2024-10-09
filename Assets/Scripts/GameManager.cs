a50e426dbb1957d078788d552dbcc02d339cce29
Kojonkoski <128294860+Kojonkoski@users.noreply.github.com>
2024-10-09 22:38:27 +0300

    commit
    Volumeslider ja bg musiikki

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,17 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
     public GameObject pauseMenuUI; // Assign the pause panel from the Unity Inspector
     private bool isPaused = false;
+    public static GameManager instance;
+    public AudioSource backgroundMusic;
+    public Slider volumeSlider;
 
     // Start is called before the first frame update
+
+    void Awake()
+    {
+
+    }
+
     void Start()
     {
-        
+        float savedVolume = PlayerPrefs.GetFloat("musicVolume", 1f);
+        SetVolume(savedVolume);
+
+        Slider[] sliders = FindObjectsOfType<Slider>();
+        foreach (Slider slider in sliders)
+        {
+            slider.value = savedVolume; // Set each slider to the saved volume
+            slider.onValueChanged.AddListener(SetVolume); // Ensure each slider updates volume
+        }
+
+        if (backgroundMusic != null && volumeSlider != null)
+        {
+            volumeSlider.value = backgroundMusic.volume; // Match slider to current volume
+            volumeSlider.onValueChanged.AddListener(SetVolume); // Add listener to handle slider changes
+        }
+
     }
 
     // Update is called once per frame
@@ -63,4 +88,13 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Reload the current scene
     }
 
+    public void SetVolume(float volume)
+    {
+        if (backgroundMusic != null)
+        {
+            backgroundMusic.volume = volume; // Set the volume on the AudioSource
+        }
+        PlayerPrefs.SetFloat("musicVolume", volume); // Save the volume setting
+        PlayerPrefs.Save(); // Ensure the data is saved
+    }
 }
