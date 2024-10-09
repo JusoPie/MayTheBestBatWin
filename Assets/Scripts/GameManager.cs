1be559dc0868778e2cc408ac9fa9361707d0578e
Kojonkoski <128294860+Kojonkoski@users.noreply.github.com>
2024-10-09 21:26:32 +0300

    commit
    pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,10 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+
+    public GameObject pauseMenuUI; // Assign the pause panel from the Unity Inspector
+    private bool isPaused = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +19,20 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        {
+            // Check for pause input (e.g., Escape key or a button click)
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (isPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
     }
 
     public void StartGame() {
@@ -25,4 +42,25 @@ public class GameManager : MonoBehaviour
     public void QuitGame() { 
     Application.Quit();
     }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);  // Hide pause menu
+        Time.timeScale = 1f;           // Resume game time
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);   // Show pause menu
+        Time.timeScale = 0f;           // Freeze game time
+        isPaused = true;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f; // Reset time scale in case it's paused
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Reload the current scene
+    }
+
 }
