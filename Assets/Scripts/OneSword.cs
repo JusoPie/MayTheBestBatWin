8df7f7aa76aed59a6b52a2b8d9aed28bc8df4173
JusoPie <128028155+JusoPie@users.noreply.github.com>
2024-10-10 12:24:57 +0300

    sword script

---
+++ Assets/Scripts/OneSword.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneSword : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;  // Speed of the sword's movement on the x-axis
+    [SerializeField] private int damage = 20;   // Amount of damage the sword deals
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the sword across the x-axis by speed every frame
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        if (transform.position.x > Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x + 1)
+        {
+            Destroy(gameObject); // Destroy the sword once it moves off the right side of the screen
+        }
+    }
+
+    // Trigger detection for hitting the player
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if the object hit is tagged as "Player"
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Attempt to get the Health component from the Player
+            Health playerHealth = collision.gameObject.GetComponent<Health>();
+
+            // If the Player has a Health component, apply damage
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damage, transform.position);
+            }
+        }
+    }
+}
+
