6c0efaaa01ee7ae4065ed3fc92497f612125e512
JusoPie <128028155+JusoPie@users.noreply.github.com>
2024-10-10 18:37:46 +0300

    Level design

--- Assets/Scripts/OneSword.cs
+++ Assets/Scripts/OneSword.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class OneSword : MonoBehaviour
 {
-    public float speed = 5f;  // Speed of the sword's movement on the x-axis
-    [SerializeField] private int damage = 20;   // Amount of damage the sword deals
+    public float speed = 5f;  
+    [SerializeField] private int damage = 20;   
 
     // Update is called once per frame
     void Update()
@@ -15,20 +15,20 @@ public class OneSword : MonoBehaviour
 
         if (transform.position.x > Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x + 1)
         {
-            Destroy(gameObject); // Destroy the sword once it moves off the right side of the screen
+            Destroy(gameObject); // Destroy the sword when offscreen
         }
     }
 
-    // Trigger detection for hitting the player
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Check if the object hit is tagged as "Player"
+        
         if (collision.gameObject.CompareTag("Player"))
         {
-            // Attempt to get the Health component from the Player
+            
             Health playerHealth = collision.gameObject.GetComponent<Health>();
 
-            // If the Player has a Health component, apply damage
+            
             if (playerHealth != null)
             {
                 playerHealth.TakeDamage(damage, transform.position);
